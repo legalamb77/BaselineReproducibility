e5807ed1899f8062a73193023a34d78012003218
Ted <theodore.morley@mail.mcgill.ca>
2018-04-19 11:24:54 -0400

    Reproduction of exact parameters from the paper in question, needs to have output classes adjusted.

---
+++ lstm_reproduction.py
@@ -0,0 +1,56 @@
+from keras.preprocessing import sequence
+from hyperopt import Trials, STATUS_OK, tpe
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+from keras import callbacks
+
+from hyperas import optim
+from hyperas.distributions import choice, uniform#, conditional
+
+max_features = 20000
+maxlen = 350
+batch_size = 128
+
+def data_wrapper():
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words = 20000)
+    x_train = sequence.pad_sequences(x_train, maxlen=350)
+    x_test = sequence.pad_sequences(x_test, maxlen=350)
+    return x_train, y_train, x_test, y_test
+
+def model_wrapper(x_train, y_train, x_test, y_test):
+    model = Sequential()
+    hiddensize = 100
+    model.add(Embedding(20000, hiddensize))
+    model.add(LSTM(hiddensize))
+    model.add(Dense(1, activation='softmax'))
+
+    model.compile(loss = 'binary_crossentropy',
+            optimizer = Adam(lr=0.0001),
+            metrics = ['accuracy'])
+    backs = [callbacks.EarlyStopping(monitor='val_loss', patience=3)]
+
+    model.fit(x_train, y_train, 
+            batch_size = 128,
+            callbacks = backs,
+            epochs = 10,
+            validation_split = 0.15)
+    score, acc = model.evaluate(x_test, y_test, verbose = 0)
+    return {'loss': -acc, 'status': STATUS_OK, 'model': model}
+
+if __name__=='__main__':
+    #best_run, best_model = optim.minimize(model = model_wrapper,
+    #                        data = data_wrapper,
+    #                        algo = tpe.suggest,
+    #                        max_evals = 1,
+    #                        trials = Trials())
+    X_train, Y_train, X_test, Y_test = data_wrapper()
+    print("Best eval: ")
+    print(Y_test[0])
+    result = model_wrapper(X_train, Y_train, X_test, Y_test)
+    #print(best_model.evaluate(X_test, Y_test))
+    print("Best parameters: ")
+    #print(best_run)
+    print(result)
